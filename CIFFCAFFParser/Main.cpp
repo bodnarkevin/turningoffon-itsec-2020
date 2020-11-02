4c3db0f93bf405f3f6bd15792d4ee887a4b09135
Kozák Ágota Boglárka <gogota97@gmail.com>
2020-11-02 22:35:54 +0100

    refactor
    Signed-off-by: Kozák Ágota Boglárka <gogota97@gmail.com>

--- CIFFCAFFParser/Main.cpp
+++ CIFFCAFFParser/Main.cpp
@@ -25,8 +25,28 @@ int convert8ByteToInteger(const std::vector<unsigned char>& buffer, int index) {
     return result;
 }
 
+void logSuccess() {
+    std::cout << std::endl << "+--------------------------------------------------+" << std::endl;
+    std::cout              << "|                                                  |" << std::endl;
+    std::cout              << "|              Process was succesful               |" << std::endl;
+    std::cout              << "|                                                  |" << std::endl;
+    std::cout              << "+--------------------------------------------------+" << std::endl;
+}
+
+void logBytesProcessed(int numberOfBytesProcessed) {
+    std::string bytesString = " byte";
+    if (numberOfBytesProcessed > 1) {
+        bytesString += "s";
+    }
+    bytesString += ".";
+    std::cout << "      Processed " << numberOfBytesProcessed << bytesString << std::endl;
+}
+
+void logMessage(std::string message) {
+    std::cout << message << std::endl;
+}
+
 void getCIFFMagic(const std::vector<unsigned char>& buffer, int index, char* result) {
-    std::cout << "getMagic..." << std::endl;
     int count = 0;
     char temp[4];
     for (int i = index; i < index + 4; i++) {
@@ -35,7 +55,7 @@ void getCIFFMagic(const std::vector<unsigned char>& buffer, int index, char* res
     }
 
     if (temp[0] != 'C' && temp[1] != 'I' && temp[2] != 'F' && temp[3] != 'F') {
-        std::cout << "CIFF magic word not found." << std::endl;
+        logMessage("ERROR: CIFF magic word not found.");
         return;
     }
 
@@ -45,7 +65,6 @@ void getCIFFMagic(const std::vector<unsigned char>& buffer, int index, char* res
 }
 
 int getCaption(const std::vector<unsigned char>& buffer, int index, std::string& result) {
-    std::cout << "getCaption..." << std::endl;
     int size = 0;
     int idx = index;
 
@@ -60,7 +79,6 @@ int getCaption(const std::vector<unsigned char>& buffer, int index, std::string&
 }
 
 void getTags(const std::vector<unsigned char>& buffer, int index, int headerLength, std::vector<std::string>& result) {
-    std::cout << "getTags" << std::endl;
     int idx = index;
 
     while (idx < index + headerLength) {
@@ -77,89 +95,141 @@ void getTags(const std::vector<unsigned char>& buffer, int index, int headerLeng
 }
 
 void getPixels(const std::vector<unsigned char>& buffer, int index, int contentLength, std::vector<uint8_t>& result) {
-    std::cout << "getPixels" << std::endl;
+    logMessage("  Getting pixels ...");
 
     for (int i = index; i < index + contentLength; i++) {
         result.push_back(static_cast<uint8_t>(buffer[i]));
     }
 }
 
-void parseCIFF(const std::vector<unsigned char>& buffer, int index, CIFF::CIFFFile& ciff) {
-    int idx = index;
-
+void parseCIFF(std::vector<unsigned char>& buffer, CIFF::CIFFFile& ciff) {
     char magic[4];
-    getCIFFMagic(buffer, idx, magic);
+    getCIFFMagic(buffer, 0, magic);
 
-    std::cout << "Magic: ";
+    std::string magicString = "    Magic: ";
     for (int i = 0; i < 4; i++) { // debug log
-        std::cout << magic[i];
+        magicString += magic[i];
     }
-    std::cout << std::endl;
+    logMessage(magicString);
 
-    idx += 4;
-    int headerLength = convert8ByteToInteger(buffer, idx);
-    idx += 8;
-    int contentLength = convert8ByteToInteger(buffer, idx);
-    idx += 8;
-    int width = convert8ByteToInteger(buffer, idx);
-    idx += 8;
-    int height = convert8ByteToInteger(buffer, idx);
-    idx += 8;
+    // Remove the parsed 4 bytes from the buffer
+    logBytesProcessed(4);
+    std::vector<unsigned char>(buffer.begin() + 4, buffer.end()).swap(buffer);
+
+    int headerLength = convert8ByteToInteger(buffer, 0);
+
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin() + 8, buffer.end()).swap(buffer);
+
+    int contentLength = convert8ByteToInteger(buffer, 0);
+
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin() + 8, buffer.end()).swap(buffer);
+
+    int width = convert8ByteToInteger(buffer, 0);
+
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin() + 8, buffer.end()).swap(buffer);
+
+    int height = convert8ByteToInteger(buffer, 0);
+
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin() + 8, buffer.end()).swap(buffer);
 
     std::string caption = "";
-    int captionLength = getCaption(buffer, idx, caption);
-    idx += captionLength;
-    std::cout << "caption: " << caption << std::endl;
+    int captionLength = getCaption(buffer, 0, caption);
+    logMessage("  Caption: " + caption);
+    // Remove the parsed captionLength bytes from the buffer
+    logBytesProcessed(captionLength);
+    std::vector<unsigned char>(buffer.begin() + captionLength, buffer.end()).swap(buffer);
 
     std::vector<std::string> tags;
     int tagsLength = headerLength - 4 - 8 - 8 - 8 - 8 - captionLength;
-    getTags(buffer, idx, tagsLength, tags);
-    idx += tagsLength;
-
-    std::cout << "tags:" << std::endl;
+    getTags(buffer, 0, tagsLength, tags);
+    std::string tagsMessage = "  Tags: ";
     for (auto element : tags) {
-        std::cout << "#" << element << std::endl;
+        tagsMessage += "#" + element + " ";
     }
+    logMessage(tagsMessage);
+    // Remove the parsed tagsLength bytes from the buffer
+    logBytesProcessed(tagsLength);
+    std::vector<unsigned char>(buffer.begin() + tagsLength, buffer.end()).swap(buffer);
 
     std::vector<uint8_t> pixels;
-    getPixels(buffer, idx, contentLength, pixels);
-    idx += contentLength;
+    getPixels(buffer, 0, contentLength, pixels);
+    logMessage("  Number of pixels: " + std::to_string(pixels.size()));
+    // Remove the parsed contentLength bytes from the buffer
+    logBytesProcessed(contentLength);
+    std::vector<unsigned char>(buffer.begin() + contentLength, buffer.end()).swap(buffer);
 
     //not logging pixels
-
-    std::cout << "position: " << idx << std::endl;
 }
 
-int handleHeader(const std::vector<unsigned char>& buffer, int index, CAFF::Block& block) {
-    std::cout << "Handling CAFF header..." << std::endl;
-    block.length = convert8ByteToInteger(buffer, index);
-    return convert8ByteToInteger(buffer, index) + 8;
+void handleHeader(std::vector<unsigned char>& buffer, CAFF::Block& block) {
+    std::cout << std::endl << "Handling CAFF header..." << std::endl;
+    int length = convert8ByteToInteger(buffer, 0);
+
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin() + 8, buffer.end()).swap(buffer);
+
+    block.length = length;
+
+    // Remove the parsed length bytes from the buffer
+    logBytesProcessed(length);
+    std::vector<unsigned char>(buffer.begin() + length, buffer.end()).swap(buffer);
+
+    std::cout << "Handled CAFF header" << std::endl << std::endl;
+
 }
 
-int handleCredits(const std::vector<unsigned char>& buffer, int index, CAFF::Block& block) {
-    std::cout << "Handling credits..." << std::endl;
-    block.length = convert8ByteToInteger(buffer, index);
-    return convert8ByteToInteger(buffer, index) + 8;
+void handleCredits(std::vector<unsigned char>& buffer, CAFF::Block& block) {
+    std::cout << std::endl << "Handling credits... " << std::endl;
+    int length = convert8ByteToInteger(buffer, 0);
+    block.length = length;
+    int fullCreditsLength = length + 8;
+
+    // Remove the processed fullCreditsLength bytes from the buffer
+    logBytesProcessed(fullCreditsLength);
+    std::vector<unsigned char>(buffer.begin() + fullCreditsLength, buffer.end()).swap(buffer);
+
+    std::cout << "Handled credits block" << std::endl << std::endl;
 }
 
-int handleAnimation(const std::vector<unsigned char>& buffer, int index, CAFF::Block& block) {
-    std::cout << "Handling animation..." << std::endl;
-    int animationLength = convert8ByteToInteger(buffer, index);
-    index += 8;
-    int duration = convert8ByteToInteger(buffer, index);
-    index += 8;
-    std::cout << "   Duration of ciff: " << duration << " ms" << std::endl;
+void handleAnimation(std::vector<unsigned char>& buffer, CAFF::Block& block) {
+    std::cout << std::endl << "Handling animation..." << std::endl;
+    int animationLength = convert8ByteToInteger(buffer, 0);
+    logMessage("  Length of animation block: " + std::to_string(animationLength));
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin()+8, buffer.end()).swap(buffer);
+
+    int duration = convert8ByteToInteger(buffer, 0);
+    logMessage("  Duration of ciff: " + std::to_string(duration) + " ms");
+    // Remove the parsed 8 bytes from the buffer
+    logBytesProcessed(8);
+    std::vector<unsigned char>(buffer.begin()+8, buffer.end()).swap(buffer);
+
+
     CIFF::CIFFFile ciff;
-    parseCIFF(buffer, index, ciff);
+    parseCIFF(buffer, ciff);
     // todo block.animation_data += ciff ?
-    return animationLength += 8;
+
+    std::cout << "Handled animation block" << std::endl << std::endl;
 }
 
-void processCAFF(const std::vector<unsigned char>& buffer, CAFF::CAFFFile& caffFile) {
-    int index = 0;
-    while (index < buffer.size()) {
-        int identifier = static_cast<int>(buffer[index]);
-        int jump = 1;
+void processCAFF(std::vector<unsigned char>& buffer, CAFF::CAFFFile& caffFile) {
+    while (buffer.size() > 0) {
+        int identifier = static_cast<int>(buffer[0]);
+
+        logMessage("Parsed block identifier: " + std::to_string(identifier));
+        // Remove the processed 1 byte from the buffer
+        logBytesProcessed(1);
+        std::vector<unsigned char>(buffer.begin() + 1, buffer.end()).swap(buffer);
 
         CAFF::Block block;
         CAFF::Header header;
@@ -170,23 +240,24 @@ void processCAFF(const std::vector<unsigned char>& buffer, CAFF::CAFFFile& caffF
         switch (identifier) {
             case CAFF::BlockType::HEADER:
                 block.header_data = &header;
-                jump = handleHeader(buffer, index + 1, block);
+                handleHeader(buffer, block);
                 break;
             case CAFF::BlockType::CREDITS:
                 block.credits_data = &credits;
-                jump = handleCredits(buffer, index + 1, block);
+                handleCredits(buffer, block);
                 break;
             case CAFF::BlockType::ANIMATION:
                 block.animation_data = &animation;
-                jump = handleAnimation(buffer, index + 1, block);
+                handleAnimation(buffer, block);
                 break;
             default:
-                std::cout << "Unknown identifier. Stopping..." << std::endl;
+                logMessage("Unknown identifier. " + std::to_string(identifier) + " Stopping...");
                 return;
         }
-        index += jump + 1;
     }
-    std::cout << "Process was succesful" << std::endl;
+
+    logSuccess();
+
 }
 
 int main() {
@@ -194,12 +265,13 @@ int main() {
     std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(source), {});
     int count = 0;
     for (auto element : buffer) {
-        std::cout << "count: " << count << " " << static_cast<int>(element) << std::endl;
+        std::cout << "Index: " << count << " " << static_cast<int>(element) << std::endl;
         if (count > 180) {
             break;
         }
         count++;
     }
+    std::cout << std::endl;
     CAFF::CAFFFile caffFile;
     processCAFF(buffer, caffFile);
 
