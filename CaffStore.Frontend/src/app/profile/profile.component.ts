c176b451993845f6b6da8ee056540511748a78c3
gnanetta <gnanetta@gmail.com>
2020-11-20 22:59:25 +0100

    profile sb

--- CaffStore.Frontend/src/app/profile/profile.component.ts
+++ CaffStore.Frontend/src/app/profile/profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+    editingInProgress: boolean = false;
 
-  ngOnInit(): void {
-  }
+    form = new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', Validators.required)
+    });
+
+    constructor() { }
+
+    ngOnInit() { }
+
+    onSaveProfile(): void {
+        this.editingInProgress = false;
+        // TODO: edit profile
+    }
+
+    onDeleteAccount(): void {
+        // TODO: delete account
+    }
 
 }
