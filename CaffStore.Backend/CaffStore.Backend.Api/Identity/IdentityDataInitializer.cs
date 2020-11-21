1231173e0ba5158edb2d487d8c427c7505d174ef
Bodnár Kevin <bodnar.kevin@gmail.com>
2020-11-21 20:48:21 +0100

    Add admin role and user seed

---
+++ CaffStore.Backend/CaffStore.Backend.Api/Identity/IdentityDataInitializer.cs
@@ -0,0 +1,56 @@
+﻿using CaffStore.Backend.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace CaffStore.Backend.Api.Identity
+{
+	public static class IdentityDataInitializer
+	{
+		public static async Task SeedDataAsync(UserManager<User> userManager, RoleManager<Role> roleManager)
+		{
+			await SeedRolesAsync(roleManager);
+			await SeedUsersAsync(userManager);
+		}
+
+		public static async Task SeedRolesAsync(RoleManager<Role> roleManager)
+		{
+			var exists = await roleManager.RoleExistsAsync(Roles.Admin);
+
+			if (exists)
+				return;
+
+			Role role = new Role
+			{
+				Name = Roles.Admin
+			};
+
+			await roleManager.CreateAsync(role);
+		}
+
+		public static async Task SeedUsersAsync(UserManager<User> userManager)
+		{
+			var adminUserName = "admin@admin";
+			var adminPassword = "Admin1234";
+
+			var exists = await userManager.FindByNameAsync(adminUserName) != null;
+
+			if (exists)
+				return;
+
+			User user = new User
+			{
+				UserName = adminUserName,
+				Email = adminUserName,
+				FirstName = "Boss",
+				LastName = "Man"
+			};
+
+			var result = await userManager.CreateAsync(user, adminPassword);
+
+			if (result.Succeeded)
+			{
+				await userManager.AddToRoleAsync(user, Roles.Admin);
+			}
+		}
+	}
+}
