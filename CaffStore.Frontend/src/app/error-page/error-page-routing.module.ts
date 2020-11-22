9e5df07693dbe8827bd365b64acce1319fda5e78
gnanetta <gnanetta@gmail.com>
2020-11-22 04:46:38 +0100

    error-page

--- CaffStore.Frontend/src/app/error-page/error-page-routing.module.ts
+++ CaffStore.Frontend/src/app/error-page/error-page-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../auth/auth-guard';
 import { ErrorPageComponent } from './error-page.component';
 
-const routes: Routes = [{ path: '', component: ErrorPageComponent }];
+const routes: Routes = [{ path: '', component: ErrorPageComponent, canActivate: [AuthGuard]  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
