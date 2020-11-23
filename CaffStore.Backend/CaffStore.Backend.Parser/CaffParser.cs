bf4b39960076e9d37053131a7cfe5eb7b02c8358
peterleist <peterle606@gmail.com>
2020-11-23 20:49:03 +0100

    Implement native dll.

--- CaffStore.Backend/CaffStore.Backend.Parser/CaffParser.cs
+++ CaffStore.Backend/CaffStore.Backend.Parser/CaffParser.cs
@@ -3,11 +3,88 @@ using System;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace CaffStore.Backend.Parser
 {
+	enum BlockType { HEADER, CREDITS, ANIMATION }
+	[StructLayout(LayoutKind.Sequential)]
+	public class CIFFHeader
+	{
+		char[] magic;
+		ulong header_size;
+		ulong content_size;
+		ulong width;
+		ulong height;
+		string caption;
+		//std::vector<std::string> tags; //TODO
+	}
+
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class CIFFFile
+	{
+		CAFFHeader header;
+		//std::vector<uint8_t> pixels; //Todo
+	};
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class CAFFHeader
+	{
+		char[] magic;
+		ulong header_size;
+		ulong num_anim;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Date
+	{
+		ushort year;
+		byte month;
+		byte day;
+		byte hour;
+		byte minute;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Credits
+	{
+		Date date;
+		ulong creator_len;
+		string creator;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Animation
+	{
+		ulong duration;
+		CIFFFile ciff_file;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Block
+	{
+		byte id;
+		ulong length;
+		CAFFHeader header_data;
+		Credits credits_data;
+		Animation animation_data;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class CAFFFile
+	{
+		int count;
+		Block[] blocks;
+	}
+
 	public static class CaffParser
 	{
+		
+		[DllImport("CAFFParser.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern CAFFFile handleParsing(string filename);
+
+
 		public static async Task<CaffParseResult> ParseCaffFileAsync(Stream fileStream)
 		{
 			// TODO fill with real data
