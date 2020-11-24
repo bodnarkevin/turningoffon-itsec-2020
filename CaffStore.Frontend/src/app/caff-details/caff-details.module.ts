af90e6b311576cb4d684aa2243dd15510099ad72
Kozák Ágota <kozak.agota@mirango.hu>
2020-11-24 13:57:21 +0100

    Comment section basics

--- CaffStore.Frontend/src/app/caff-details/caff-details.module.ts
+++ CaffStore.Frontend/src/app/caff-details/caff-details.module.ts
@@ -8,7 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [CaffDetailsComponent],
@@ -19,7 +20,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class CaffDetailsModule { }
