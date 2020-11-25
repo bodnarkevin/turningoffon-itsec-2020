48eb7f64099a6f298d7300fb5f2c93984df55b81
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2020-11-25 15:15:47 +0100

    [Parser] CAFF Parser WIP #1

--- CaffStore.Backend/CaffStore.Backend.Parser/CaffParser.cs
+++ CaffStore.Backend/CaffStore.Backend.Parser/CaffParser.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
 
 namespace CaffStore.Backend.Parser
 {
@@ -80,45 +81,101 @@ namespace CaffStore.Backend.Parser
 
 	public static class CaffParser
 	{
-		
-		[DllImport("CAFFParser.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-		private static extern CAFFFile handleParsing(string filename);
+
+		[DllImport("C:/Users/cloud/source/repos/turningoffon-itsec-2020/CaffStore.Backend/Debug/CaffStore.Backend.Parser.Native.dll", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.StdCall)]
+		[return: MarshalAs(UnmanagedType.LPStr)]
+		public static extern string parseToJson(IntPtr pArray, int nSize, out IntPtr preview, out int size, out bool isError);
+
+		private static byte[] ReadBytes(Stream input)
+        {
+			byte[] buffer = new byte[16 * 1024];
+			using (MemoryStream ms = new MemoryStream())
+			{
+				int read;
+				while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+				{
+					ms.Write(buffer, 0, read);
+				}
+
+				return ms.ToArray();
+			}
+		}
 
 
 		public static async Task<CaffParseResult> ParseCaffFileAsync(Stream fileStream)
 		{
 			// TODO fill with real data
+			byte[] buffer = ReadBytes(fileStream);
+			int size = Marshal.SizeOf(buffer[0]) * buffer.Length;
+			IntPtr pnt = Marshal.AllocHGlobal(size);
 
-			var tags = new[] { "tag1", "tag2", "tag3" };
-			var ciffData = new CiffDataDto
-			{
-				Caption = "ciff1",
-				Height = 100,
-				Width = 200,
-				Tags = tags,
-			};
-			var animation = new CaffAnimationDataDto
-			{
-				Order = 1,
-				Duration = 500,
-				CiffData = ciffData,
-			};
-			var caffData = new CaffDataDto
-			{
-				Creator = "creator1",
-				Creation = DateTime.Now,
-				Animations = new[] { animation },
-			};
-			var preview = new Bitmap(100, 200);
-			preview.SetPixel(1, 1, Color.Red);
+			try
+            {
+				Marshal.Copy(buffer, 0, pnt, buffer.Length);
+            } catch
+            {
+				//Free unmanaged memory
+            }
 
-			return new CaffParseResult
+			bool error = true;
+			int preSize;
+			IntPtr preview;
+
+			string parsedJson = parseToJson(pnt, buffer.Length, out preview, out preSize, out error);
+
+			if (error)
 			{
-				Succeeded = true,
-				Message = "Success",
-				Preview = preview,
-				Result = caffData,
-			};
+				var tags = new[] { "tag1", "tag2", "tag3" };
+				var ciffData = new CiffDataDto
+				{
+					Caption = "ciff1",
+					Height = 100,
+					Width = 200,
+					Tags = tags,
+				};
+				var animation = new CaffAnimationDataDto
+				{
+					Order = 1,
+					Duration = 500,
+					CiffData = ciffData,
+				};
+				var caffData = new CaffDataDto
+				{
+					Creator = "creator1",
+					Creation = DateTime.Now,
+					Animations = new[] { animation },
+				};
+				var previewDummy = new Bitmap(100, 200);
+				previewDummy.SetPixel(1, 1, Color.Red);
+
+				return new CaffParseResult
+				{
+					Succeeded = false,
+					Message = "Failure",
+					Preview = previewDummy,
+					Result = caffData,
+				};
+			} else
+            {
+				CaffDataDto caffDataFromParser = JsonConvert.DeserializeObject<CaffDataDto>(parsedJson);
+
+				byte[] previewArray = new byte[preSize];
+				Marshal.Copy(preview, previewArray, 0, preSize);
+				Bitmap previewBitmap;
+				using (var ms = new MemoryStream(previewArray))
+				{
+					previewBitmap = new Bitmap(ms);
+				}
+				Marshal.FreeHGlobal(pnt);
+
+				return new CaffParseResult
+				{
+					Succeeded = true,
+					Message = "Success",
+					Preview = previewBitmap,
+					Result = caffDataFromParser
+				};
+            }
 		}
 	}
 }
