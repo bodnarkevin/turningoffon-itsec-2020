beaad368d18881ed32be0af9499c34e7286cd8d7
Gonda-Nagy Anetta <gnanetta@gmail.com>
2020-11-27 21:47:47 +0100

    fix console error

--- CaffStore.Frontend/src/app/auth/auth.service.ts
+++ CaffStore.Frontend/src/app/auth/auth.service.ts
@@ -2,43 +2,44 @@ import { Injectable } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AuthService {
-  constructor(private oAuthService: OAuthService) {}
+    
+    constructor(private oAuthService: OAuthService) { }
 
-  public getAccessToken(): string {
-    return this.oAuthService.getAccessToken();
-  }
+    public getAccessToken(): string {
+        return this.oAuthService.getAccessToken();
+    }
 
-  public isLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
-  }
+    public isLoggedIn(): boolean {
+        return this.oAuthService.hasValidAccessToken();
+    }
 
-  async isAdmin(): Promise<boolean> {
-    return this.oAuthService.loadUserProfile().then((res) => {
-      if (res && res.id) {
-        console.log(res.id);
-      } else {
-        console.log(res);
-      }
-      if (res && res.role && res.role === 'Admin') {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
+    async isAdmin(): Promise<boolean> {
+        return await this.oAuthService.loadUserProfile()
+            .then((res) => {
+                    if (res && res.role && res.role === 'Admin') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+            .catch((err) => {
+                    console.log(err);
+                    return false;
+                });
+    }
 
-  async getCurrentUserEmail(): Promise<string> {
-    return this.oAuthService.loadUserProfile().then((res) => {
-      if (res && res.name) {
-        const email = res.name;
-        console.log(email);
-        return email;
-      } else {
-        console.log('email not found');
-      }
-    });
-  }
+    async getCurrentUserEmail(): Promise<string> {
+        return await this.oAuthService.loadUserProfile().then((res) => {
+            if (res && res.name) {
+                const email = res.name;
+                console.log(email);
+                return email;
+            } else {
+                console.log('email not found');
+            }
+        });
+    }
 }
