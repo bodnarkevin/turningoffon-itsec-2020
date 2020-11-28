20afd56397d048d1b14c8431a7946348922a4e07
Bakai István Bálint <bakai.istvan.lfgeb@gmail.com>
2020-11-28 09:19:52 +0100

    Valgrind fix #1.3

--- CIFFCAFFParser/CAFFHandler.h
+++ CIFFCAFFParser/CAFFHandler.h
@@ -11,32 +11,32 @@ enum BlockType { HEADER = 1, CREDITS = 2, ANIMATION = 3};
 
 struct Header {
     char magic[4];
-    uint64_t header_size;
-    uint64_t num_anim;
+    uint64_t header_size = 0;
+    uint64_t num_anim = 0;
 };
 
 struct CaffDate {
-    uint16_t year;
-    uint8_t month;
-    uint8_t day;
-    uint8_t hour;
-    uint8_t minute;
+    uint16_t year = 0;
+    uint8_t month = 0;
+    uint8_t day = 0;
+    uint8_t hour = 0;
+    uint8_t minute = 0;
 };
 
 struct Credits {
     CaffDate date;
-    uint64_t creator_len;
+    uint64_t creator_len = 0;
     std::string creator;
 };
 
 struct Animation {
-    uint64_t duration;
+    uint64_t duration = 0;
     CIFF::CIFFFile ciff_file;
 };
 
 struct Block {
-    uint8_t id;
-    uint64_t length;
+    uint8_t id = 0;
+    uint64_t length = 0;
     Header header_data;
     Credits credits_data;
     Animation animation_data;
