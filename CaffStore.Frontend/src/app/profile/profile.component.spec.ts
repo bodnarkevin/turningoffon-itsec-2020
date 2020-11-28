3238d48e144f41bce4b5f5e56a95b115e27dbe06
Kozák Ágota Boglárka <gogota97@gmail.com>
2020-11-28 21:59:20 +0100

    Add some tests

--- CaffStore.Frontend/src/app/profile/profile.component.spec.ts
+++ CaffStore.Frontend/src/app/profile/profile.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { OAuthModule } from 'angular-oauth2-oidc';
+import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
 
 import { ProfileComponent } from './profile.component';
 
